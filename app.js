4bac8110c0ee7bd3b9d63b2fcf526b1619b4f606
steepllama <steepllama@gmail.com>
2022-01-12 15:00:23 -0600

    cleaned up the code for the app.js file and created a new file for cleaner code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+// const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site');
 const generatePage = require('./src/page-template');
 const inquirer = require ('inquirer');
 
@@ -129,52 +130,18 @@ if (!portfolioData.projects) {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData);
-  const pageHTML = generatePage(portfolioData);
-  fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw new Error (err);
+  return generatePage(portfolioData);
+})
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-});
-
-  //     console.log('Page created! Check out index.html in this directory to see it!');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
