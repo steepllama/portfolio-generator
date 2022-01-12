2fd7fe503a9805285de4c7ce96c1209996e4a0d6
steepllama <steepllama@gmail.com>
2022-01-11 20:32:00 -0600

    updated the node with the html format for multiple  project viewing, added the button with a ref link taking you to the Github repo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const inquirer = require ('inquirer');
 const generatePage = require('./src/page-template');
+const inquirer = require ('inquirer');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -130,12 +130,13 @@ promptUser()
 .then(promptProject)
 .then(portfolioData => {
   console.log(portfolioData);
-  // const pageHTML = generatePage(portfolioData);
-  // fs.writeFile('./index.html', pageHTML, err => {
-  //     if (err) throw new Error (err);
-  //     console.log('Page created! Check out index.html in this directory to see it!');
-  //});
+  const pageHTML = generatePage(portfolioData);
+  fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error (err);
+  });
 });
+
+  //     console.log('Page created! Check out index.html in this directory to see it!');
 
 
 
